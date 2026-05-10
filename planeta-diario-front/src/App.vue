87bb10051df0ffe66161ca76f0026ea276dcc504
Ricardo Fragoso <ricardofsilva0412@gmail.com>
2026-05-10 20:02:11 -0300

    chore: inicio da configuracao frontend vueJS

---
+++ planeta-diario-front/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen bg-gray-900 flex items-center justify-center">
+    <h1 class="text-4xl font-bold text-blue-400">
+      Planeta Diário Frontend Online! 🌍
+    </h1>
+  </div>
+</template>
