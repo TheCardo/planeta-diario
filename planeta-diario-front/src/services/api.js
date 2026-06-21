a576d07a9d487f6581162fc758904398ed2e9b1b
Ricardo Fragoso <ricardofsilva0412@gmail.com>
2026-06-21 01:26:14 -0300

    config: atualiza url da api para o novo ip da aws

--- planeta-diario-front/src/services/api.js
+++ planeta-diario-front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://3.94.186.202:8080',
+  baseURL: 'http://3.84.155.169:8080',
 });
 
 api.interceptors.request.use((config) => {
